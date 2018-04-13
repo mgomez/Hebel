cc42a86105f25d1020bb9fbce8448643369aa3e8
Manuel Gomez <00.13.zkr@gmail.com>
2018-04-13 12:31:12 -0500

    init

---
+++ www/index.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import "./css/app.less";
+import page from "./views/page.html";
+import $ from "jquery";
+
+$("#page").html(page);
+
+export default {
+	// initialization page
+	init() {}
+};
