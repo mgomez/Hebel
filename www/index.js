a924fd73dcaabe0a04a6bc1290908cc19e96f70d
Cesar Gomez <00.13.zkr@gmail.com>
2018-05-07 22:00:27 -0500

    ajax contacto

--- www/index.js
+++ www/index.js
@@ -11,52 +11,62 @@ $("#page").html(page);
 
 
 $(function() {
-	AOS.init({
-		disable: 'mobile'
-	});
+    AOS.init({
+        disable: 'mobile'
+    });
 
-	FastClick.attach(document.body);
+    FastClick.attach(document.body);
 
-	//scroll
-	$('a[href*=\\#]').on('click', function(event) {
-		event.preventDefault();
-		$('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
-	});
+    //scroll
+    $('a[href*=\\#]').on('click', function(event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+    });
 
-	var scrollTop = $(".scrollTop");
+    var scrollTop = $(".scrollTop");
 
-	$(window).scroll(function() {
+    $(window).scroll(function() {
 
-		var topPos = $(this).scrollTop();
+        var topPos = $(this).scrollTop();
 
-		if (topPos > 100) {
-			$(scrollTop).css("opacity", "1");
-		} else {
-			$(scrollTop).css("opacity", "0");
-		}
+        if (topPos > 100) {
+            $(scrollTop).css("opacity", "1");
+        } else {
+            $(scrollTop).css("opacity", "0");
+        }
 
-	});
+    });
 
-	//Click event to scroll to top
-	$(scrollTop).click(function() {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 800);
-		return false;
+    //Click event to scroll to top
+    $(scrollTop).click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
 
-	});
+    });
 
-	$("#frm-contacto").on("submit", function() {
-		var $frm = $(this);
-		if ($frm.valid()) {
-			alert("Se envio correctamente la informacion");
-			$frm[0].reset();
-		}
-		return false;
-	}).validate();
+    $("#frm-contacto").on("submit", function() {
+        var $frm = $(this);
+        if ($frm.valid()) {
+
+            $.ajax({
+                    url: $frm.prop("action"),
+                    type: 'POST',
+                    dataType: 'json',
+                    data: $frm.serialize(),
+                })
+                .done(function() {
+                    console.log("success");
+                    alert("Se envio correctamente la informacion");
+                    $frm[0].reset();
+                });
+        }
+        return false;
+    }).validate();
 });
 
 export default {
-	// initialization page
-	init() {}
+    // initialization page
+    init() {}
 };
